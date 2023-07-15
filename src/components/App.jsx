efa947d263d7f064f8615accc1be0ce4c1dfd36e
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-07-15 15:10:24 +0300

    abracadabra bl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { Component } from "react";
 import axios from "axios";
 import Notiflix from "notiflix";
@@ -8,93 +6,122 @@ import { BASE_URL, API_KEY, SEARCH_PARAMS } from "./Pixabay/Pixabay";
 import Searchbar from "./Searchbar/Searchbar";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
-import LoadMoreButton from "./Button/Button"
-import SpinnerLoader from "./Loader/Loader"
+import LoadMoreButton from "./Button/Button";
+import SpinnerLoader from "./Loader/Loader";
 import Modal from "./Modal/Modal";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hits: [],
+      name: "",
+      page: 1,
+      showModal: false,
+      loading: false,
+      largeImageURL: "",
+      tags: "",
+      totalHits: 0,
+    };
+  }
 
- class App extends Component {
+  componentDidMount() {
+   
+  }
 
-  state = {
-    hits: [],
-    name: '',
-    page: 1,
-    showModal: false,
-    loading: false,
-    largeImegeURL: '',
-    tags: '',
-  };
+  componentDidUpdate(prevProps, prevState) {
+    const { name, page } = this.state;
+    if (prevState.name !== name || prevState.page !== page) {
+      this.fetchImages();
+    }
+  }
 
   toggleModal = (imageURL, tag) => {
-    this.setState(({showModal}) => ({
+    this.setState(({ showModal }) => ({
       showModal: !showModal,
-      largeImegeURL: imageURL,
+      largeImageURL: imageURL,
       tags: tag,
     }));
   };
 
-  getValue = ({name, page}) => {
-    this.setState({loading: true});
+  getValue = ({ name }) => {
+    this.setState({
+      hits: [],
+      name,
+      page: 1,
+      totalHits: 0,
+    });
+  };
+
+  
+
+  fetchImages = () => {
+    const { name, page } = this.state;
+    this.setState({ loading: true });
     try {
       axios
         .get(
           `${BASE_URL}?key=${API_KEY}&q=${name}&page=${page}&${SEARCH_PARAMS}`
         )
-        .then(response => {
+        .then((response) => {
           if (!response.data.hits.length) {
-            Notiflix.Notify.failure('No images found!');
+            Notiflix.Notify.failure("No images found!");
           }
-          else if (name === this.state.name) {
-            this.setState(state => ({
-              hits: [...state.hits, ...response.data.hits],
-              name: name,
-              page: state.page + 1,
-              loading: false,
-            }));
-          }
-          else {
-            this.setState(state => ({
-              hits:response.data.hits,
-              name: name,
-              page: state.page + 1,
-              loading: false,
-            }));
-          }
+          const modifiedHits = response.data.hits.map(
+            ({ id, tags, webformatURL, largeImageURL }) => ({
+              id,
+              tags,
+              webformatURL,
+              largeImageURL,
+            })
+          );
+          this.setState((prevState) => ({
+            hits: [...prevState.hits, ...modifiedHits],
+            totalHits: response.data.totalHits,
+            loading: false,
+          }));
         });
     } catch (error) {
-        console.error(error.message);
-    } 
+      console.error(error.message);
+      this.setState({ loading: false });
+    }
   };
-
-
-
+  
   loadMore = () => {
-    this.getValue(this.state);
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
   };
 
-
-  render () {
-    const {hits, showModal, loading, largeImegeURL, tags} = this.state;
+  render() {
+    const {
+      hits,
+      showModal,
+      loading,
+      largeImageURL,
+      
+      totalHits,
+    } = this.state;
 
     return (
       <div>
+        <Searchbar onSubmitHandler={this.getValue} />
 
-         <Searchbar onSubmitHandler={this.getValue} />
+        {hits.length > 0 && (
+          <ImageGallery>
+            <ImageGalleryItem articles={hits} onImage={this.toggleModal} />
+          </ImageGallery>
+        )}
 
-         {hits && (
-           <ImageGallery>
-             <ImageGalleryItem articles={hits} onImage={this.toggleModal} />
-           </ImageGallery>
-         )}
+        {showModal && (
+          <Modal onClose={this.toggleModal} url={largeImageURL} />
+        )}
 
-         {showModal && (<Modal onClose={this.toggleModal} url={largeImegeURL} alt={tags} />)}
+        {loading && <SpinnerLoader />}
 
-         {loading && <SpinnerLoader />}
-
-         {hits.length > 0 && (
-          <LoadMoreButton onButtonClick={() => this.loadMore()} />
-         )}
-
+        {totalHits > 0 && hits.length < totalHits && (
+          <LoadMoreButton onButtonClick={this.loadMore} />
+        )}
       </div>
     );
   }
